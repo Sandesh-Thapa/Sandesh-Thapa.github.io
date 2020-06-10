b6ceb02a9898e90dd6d41579d07287771a8876c4
Sandesh Thapa <sandeshthapa426@gmail.com>
2020-06-10 13:49:11 +0545

    Iniial Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//alert("Javascript Loading");
+
+var image = document.getElementById("githubPic");
+var gitlink = document.getElementById("githubLink");
+var repos = document.getElementById("tableHead");
+var repoTable = "";
+
+fetch("https://api.github.com/users/Sandesh-Thapa")
+  .then((response) => response.json())
+  .then(function (data) {
+    //console.log(data);
+    image.src = data["avatar_url"];
+    gitlink.href = data["html_url"];
+  });
+
+fetch("https://api.github.com/users/Sandesh-Thapa/repos")
+  .then((response) => response.json())
+  .then(function (data) {
+    console.log(data);
+    data.forEach((element) => {
+      repoTable += `<tr>
+                        <td>${element.name}</td>
+                        <td><a href="${element.html_url}" target="_blank"><i class="fa fa-code" aria-hidden="true"></i> View Code</a></td>
+                        <td>${element.language}</td>
+                        <td><a href="${element.homepage}" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i> View Live</a></td>
+                      </tr>`;
+    });
+    repos.innerHTML = repoTable;
+  });
