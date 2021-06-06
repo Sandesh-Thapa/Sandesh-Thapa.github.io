1c5dc077a3885cef4b43a48142d8b0e213ea94aa
Sandesh-Thapa <sandeshthapa426@gmail.com>
2021-06-06 18:03:25 +0545

    Bug Fixes - project live url

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ fetch("https://api.github.com/users/Sandesh-Thapa/repos")
   .then((response) => response.json())
   .then(function (data) {
     data.forEach((element) => {
-      var homepage = element.homepage != null ? '<td><a href="${element.homepage}" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i> View Live</a></td>' : '<td><p> - </p></td>';
+      var homepage = element.homepage != null ? `<td><a href="${element.homepage}" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i> View Live</a></td>` : '<td><p> - </p></td>';
       repoTable += `<tr>
                         <td>${element.name}</td>
                         <td><a href="${element.html_url}" target="_blank"><i class="fa fa-code" aria-hidden="true"></i> View Code</a></td>
