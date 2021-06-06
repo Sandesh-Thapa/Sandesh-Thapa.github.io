c74a11587528d5c9b920782f104d06ab315900c0
Sandesh-Thapa <sandeshthapa426@gmail.com>
2021-06-06 17:53:45 +0545

    [Fixed] Project div scroll, grayscale image and google iframe

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var repoTable = "";
 fetch("https://api.github.com/users/Sandesh-Thapa")
   .then((response) => response.json())
   .then(function (data) {
-    //console.log(data);
     image.src = data["avatar_url"];
     gitlink.href = data["html_url"];
   });
@@ -16,13 +15,13 @@ fetch("https://api.github.com/users/Sandesh-Thapa")
 fetch("https://api.github.com/users/Sandesh-Thapa/repos")
   .then((response) => response.json())
   .then(function (data) {
-    console.log(data);
     data.forEach((element) => {
+      var homepage = element.homepage != null ? '<td><a href="${element.homepage}" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i> View Live</a></td>' : '<td><p> - </p></td>';
       repoTable += `<tr>
                         <td>${element.name}</td>
                         <td><a href="${element.html_url}" target="_blank"><i class="fa fa-code" aria-hidden="true"></i> View Code</a></td>
                         <td>${element.language}</td>
-                        <td><a href="${element.homepage}" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i> View Live</a></td>
+                        ${homepage}
                       </tr>`;
     });
     repos.innerHTML = repoTable;
